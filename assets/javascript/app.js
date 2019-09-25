59c27a78032c02069501685483d3af6197d1b9fe
ALayendecker <alcommits@gmail.com>
2019-09-25 16:09:27 -0500

    everything but grading working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,15 +23,23 @@
 
 // modified timer origional found on jsFiddle made by Srinivas Chekuri
 function grading() {
-  if ($(".aq1.selected").attr("data-value") == "Iron man");
-  console.log($(".aq1.selected").attr("data-value"));
-  correct++;
-  if ($(".aq2.selected").attr("data-value") == "Billy Corgan");
-  console.log($(".aq1.selected").attr("data-value"));
-  correct++;
-  if ($(".aq3.selected").attr("data-value") == "Sting");
-  console.log($(".aq1.selected").attr("data-value"));
-  correct++;
+  if ($("#q1.aq1.selected").attr("data-value") == "Iron man") {
+    correct++;
+    console.log(correct);
+    // console.log($(".aq1.selected").attr("data-value"));
+  }
+  if ($("#q2.aq2.selected").attr("data-value") == "Billy Corgan") {
+    correct++;
+    console.log(correct);
+    // console.log($(".aq2.selected").attr("data-value"));
+  }
+  if ($("#q3.aq3.selected").attr("data-value") == "Sting") {
+    correct++;
+    console.log(correct);
+  } else {
+    wrong++;
+  }
+  // console.log($(".aq3.selected").attr("data-value"));
 }
 
 var counter = 30;
@@ -46,39 +54,46 @@ var interval = setInterval(function() {
     $("#time").text(counter);
     //console.log("Time Left --> " + counter);
 
-    // if (counter === 0) {
-    //   grading();
-    // }
+    if (counter === 15) {
+      grading();
+    }
   }
 }, 1000);
 
 var correct = 0;
+var wrong = 0;
 // var userAnswers = $("button").click();
 
-$(document).on("click", "input", function() {
+$("#q1").on("click", "input", function() {
   $(this).addClass("selected");
   $(this)
     .siblings()
     .removeClass("selected");
 
-  console.log($(".aq1.selected").attr("data-value"));
+  if ($(".aq1").hasClass("selected")) {
+    console.log($(".aq1.selected").attr("data-value"));
+  }
 });
 
-$(document).on("click", "input", function() {
+$("#q2").on("click", "input", function() {
   $(this).addClass("selected");
   $(this)
     .siblings()
     .removeClass("selected");
 
-  console.log($(".aq2.selected").attr("data-value"));
+  if ($(".aq1").hasClass("selected")) {
+    console.log($(".aq2.selected").attr("data-value"));
+  }
 });
 
-$(document).on("click", "input", function() {
+$("#q3").on("click", "input", function() {
   $(this).addClass("selected");
   $(this)
     .siblings()
     .removeClass("selected");
 
-  console.log($(".aq3.selected").attr("data-value"));
+  if ($(".aq1").hasClass("selected")) {
+    console.log($(".aq3.selected").attr("data-value"));
+  }
 });
 $("#grading").html("Correct: " + correct);
